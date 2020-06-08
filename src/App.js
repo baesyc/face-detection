a0c688e11d827d20bb6d48a8e98c13fe64b57eb8
Benjamin Cho <bycho1991@gmail.com>
2020-06-07 22:01:00 -0500

    added face detection features

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+
+const app = new Clarifai.App({
+  apiKey: 'de5f132e001a4e2cb54fefe6971abf45'
+});
 
 const particlesOptions = {
   particles: {
@@ -21,10 +28,50 @@ const particlesOptions = {
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    //assignts the percentages of the face deteced relative to image
+    const face = data.outputs[0].data.regions[0].region_info.bounding_box;
+    
+    //grabs the image provided
+    const image = document.querySelector('#inputimage');
+
+    //grabs the width and height of the image provided
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: face.left_col * width,
+      topRow: face.top_row * height,
+      rightCol: width - (face.right_col * width),
+      bottomRow: height - (face.bottom_row * height)
+    }
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (e) => {
+    this.setState({input: e.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
-
     return (
       <div className="app tc">
         <Particles
@@ -34,8 +81,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     <div className="ma4 mt0 tc">
       <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3" >
-          <img style={{paddingTop: '1px'}} src="https://img.icons8.com/ios/150/000000/brain.png"/>
+          <img style={{paddingTop: '1px'}} src="https://img.icons8.com/ios/150/000000/brain.png" alt='' />
         </div>
       </Tilt>
     </div>
